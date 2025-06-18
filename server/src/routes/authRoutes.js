4989459c1c9757e31a0c841f594147125170f324
sanjiep.magar <saasmagar554@gmail.com>
2025-06-18 11:08:06 +0300

    schema,register routes created and jwtToken added

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,12 +1,69 @@
 import express from 'express';
+import User from '../models/User.js';
+import generateToken from '../lib/jwt.js';
 
 const router = express.Router();
 
-router.post('/register', async(req, res) => {
-    res.send('Regsiter endpoint');
+router.post('/register', async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        if (!email || !username || !password) {
+            return res.status(400).json({ message: 'All fields are required' });
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({ message: 'Password must be at least 6 characters long' });
+        }
+
+        if (username.length < 3) {
+            return res.status(400).json({ message: 'Username must be at least 3 characters long' });
+        }
+
+        // Check if username already exists
+        const existingUsername = await User.findOne({ $or: [{ username }] });
+
+        if (existingUsername) {
+            return res.status(400).json({ message: 'Username already exists' });
+        }
+
+        // Check if email already exists
+        const existingEmail = await User.findOne({ $or: [{ email }] });
+
+        if (existingEmail) {
+            return res.status(400).json({ message: 'Email already exists' });
+        }
+
+        // generating random user profile image
+        const profileImage = `https://api.dicebear.com/7.x/adventurer/svg?seed=${username}`;
+
+        // Create a new user
+        const newUser = new User({
+            username,
+            email,
+            password,
+            profileImage,
+        });
+
+        await newUser.save();
+
+        const token = generateToken(newUser._id);
+        res.status(201).json({
+            token,
+            user: {
+                id: newUser._id,
+                username: newUser.username,
+                email: newUser.email,
+                profileImage: newUser.profileImage
+            },
+        });
+
+    } catch (error) {
+        console.error('Error in register:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 })
 
-router.post('/login', async(req, res) => {
+router.post('/login', async (req, res) => {
     res.send('Login endpoint');
 })
 
